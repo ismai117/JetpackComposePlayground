f8be4ad330667a6fe5e6d2ef92e3e38ef21e1cd2
ismail <ismail1973x@gmail.com>
2021-10-10 12:59:01 +0100

    first commit

---
+++ app/src/main/java/com/im/jetpackcomposeplayground/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.im.jetpackcomposeplayground
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.im.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
+import java.time.format.TextStyle
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContent {
+
+        }
+
+    }
+}
+
+
+
+@Composable
+fun ImageCard(){
+
+
+
+
+
+
+}
